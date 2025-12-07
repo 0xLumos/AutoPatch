537c66b5f9ef750e1d9fcc2a850f7605eb3fbb3f
Nour Alhouseini <86202341+0xLumos@users.noreply.github.com>
2025-12-07 06:00:38 -0500

    Delete v1

--- src/comparer.py
+++
@@ -1,9 +0,0 @@
-def compare(before: dict, after: dict) -> dict:
-    out = {}
-    for k in before.keys():
-        out[k] = {
-            "before": before[k],
-            "after": after[k],
-            "reduction": before[k] - after[k]
-        }
-    return out
